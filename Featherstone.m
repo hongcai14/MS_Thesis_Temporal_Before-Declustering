1a4b098d0c50402d48ac349dddaedd2a88efab14
hongcai14 <131423443+hongcai14@users.noreply.github.com>
2023-04-21 11:33:53 -0400

    Add files via upload

--- Featherstone.m
+++ Featherstone.m
@@ -1,13 +1,18 @@
 function[X,Y,Z] = Featherstone(data,i,j,k)
 %==========================================================================
-%Conversion from Geodetic (WGS84) to Cartesian coordinates based on Gerdan
-%Deakin 1999 and Featherstone 1994 (pp. 8-10)
-
-%   data := catalogue
-%   i, j, k := column # of latitude, longitude, and depth, respectively
-
-%   X, Y, Z := Cartesian coordinates of hypocenters
-
+%   
+%   Coordinate conversion of earthquake hypocenters from Geodetic (WGS84)
+%   to Cartesian coordinates (Featherstone, 1994; Gerdan and Deakin, 1999)
+%
+%   Input:
+%    data := earthquake catalogue, where latitude and longitude are in
+%    decimal degrees and depth in km (negative if "above ground")
+%    i, j, k := column number of latitude, longitude, and depth,
+%    respectively
+%
+%   Output:
+%    X, Y, Z := Cartesian coordinates of earthquake hypocenters 
+% 
 %==========================================================================
 
 a = 6378.137;
@@ -16,21 +21,17 @@ f = 1/298.257223563;
 %^flattening 
 
 lat = data(:,i);
-%^latitude in degrees
 lon = data(:,j);
-%^longitude in degrees
-%R = sqrt(((a^2*cosd(lat)).^2 + (b^2*sind(lat)).^2)./((a*cosd(lat)).^2+(b*sind(lat)).^2));
-%height of WGS84 in km at latitude lat 
 h = -data(:,k);
-%^height in km 
+%^convert depth to height 
 
-HJC = f*(2 - f);
-%^in the paper, HJC = e^2 := square of the first eccentricity 
-v = a./sqrt(1 - HJC*sind(lat).*sind(lat));
+ff = f*(2 - f);
+%^in the paper, ff = e^2 := square of the first eccentricity 
+v = a./sqrt(1 - ff*sind(lat).*sind(lat));
 %^the radius of the curvature in the prime vertical plane 
 
 X = (v+h).*cosd(lat).*cosd(lon);
 Y = (v+h).*cosd(lat).*sind(lon);
-Z = (v.*(1 - HJC)+h).*sind(lat);
+Z = (v.*(1 - ff)+h).*sind(lat);
 
 end
