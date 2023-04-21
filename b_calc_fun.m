e2471af5940a200c5ad24e04e5638a0c8388ec11
hongcai14 <131423443+hongcai14@users.noreply.github.com>
2023-04-21 11:50:45 -0400

    Add files via upload

---
+++ b_calc_fun.m
@@ -0,0 +1,19 @@
+function[b] = b_calc_fun(M)
+%==========================================================================
+%
+%   Computes the maximum likelihood b value (Ogata and Yamashina, 1986)
+%
+%   Input:
+%    M := vector of earthquake magnitudes 
+%
+%   Output:
+%    b := maximum likelihood b value
+%   
+%==========================================================================
+
+N = length(M);
+M_0 = min(M);
+
+b = N*log10(exp(1))/sum(M - M_0);
+
+end
